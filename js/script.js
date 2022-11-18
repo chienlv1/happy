255cee741b64a5d80637d0a31b06e0c3a68ab4bf
LeChien <chien@LeChiens-MacBook-Pro.local>
2022-11-19 01:02:15 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,55 @@
+function countdown() {
+  const nums = document.querySelectorAll(".nums span");
+  nums.forEach((num, idx) => {
+    const penultimate = nums.length - 1;
+    num.addEventListener("animationend", (e) => {
+      if (e.animationName === "goIn" && idx !== penultimate) {
+        num.classList.remove("in");
+        num.classList.add("out");
+      } else if (e.animationName === "goOut" && num.nextElementSibling) {
+        num.nextElementSibling.classList.add("in");
+      }
+      if (+idx === 2) {
+        document.getElementById("js-counter").classList.add("hide-element");
+        document.getElementById("js-heart").classList.add("show-element");
+        document
+          .getElementById("js-heart")
+          .addEventListener("click", onClickHeart);
+      }
+    });
+  });
+}
+function onClickHeart() {
+  document.getElementById("js-wrapper-counter").classList.add("hide-element");
+  document.getElementById("js-birthday").classList.add("show-element");
+  var audio = document.getElementById("audio");
+  if (audio) {
+    audio.play();
+    audio.muted = false;
+  }
+  document.getElementById("js-heart").classList.remove("show-element");
+}
+function addStar() {
+  var s = document.createElement("div");
+  s.className = "star";
+  s.style.setProperty("--size", Math.random() * 10 + 3 + "vmin");
+  s.style.left = Math.floor(Math.random() * 100) + "%";
+  s.style.top = Math.floor(Math.random() * 100) + "%";
+  s.onanimationend = function () {
+    this.remove();
+  };
+  document.body.appendChild(s);
+}
+
+function clickAudio() {
+  var audio = document.getElementById("audio");
+  audio.play();
+  audio.muted = false;
+}
+setInterval(addStar, 50);
+window.onload = function () {
+  // when the sound has been loaded, execute your code
+  clickAudio();
+  countdown();
+  addStar();
+};
